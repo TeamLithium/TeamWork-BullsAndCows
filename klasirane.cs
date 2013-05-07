d1092b35a008764f5f6b80b37e9eb9b04e77f462
kalin4o <piiz69@yahoo.com>
2013-05-07 23:57:30 +0300

    formating

--- klasirane.cs
+++ klasirane.cs
@@ -7,32 +7,37 @@ public class klasirane
     private const int KLASIRANE_RAZMER = 5;
     private static klasirane instance;
     private List<KeyValuePair<string, int>> klasiraneto;
+
     private klasirane()
     {
         this.klasiraneto = new List<KeyValuePair<string, int>>();
     }
+
     private void Sort()
     {
-        this.klasiraneto.Sort(new Comparison<KeyValuePair<string, int>>(
-                                                                        (first, second) => second.Value.CompareTo(first.Value)
-                                                                        ));
+        this.klasiraneto.Sort(new Comparison<KeyValuePair<string, int>>((first, second) => second.Value.CompareTo(first.Value)));
     }
+
     public bool IsHighScore(int attempts)
     {
         if (this.klasiraneto.Count < KLASIRANE_RAZMER || this.klasiraneto.Last().Value < attempts)
         {
             return true;
         }
+
         return false;
     }
+
     public static klasirane GetInstance()
     {
         if (instance == null)
         {
             instance = new klasirane();
         }
+
         return instance;
     }
+
     public void Add(string name, int attempts)
     {
         this.klasiraneto.Add(new KeyValuePair<string, int>(name, attempts));
@@ -43,6 +48,7 @@ public class klasirane
         }
 
     }
+
     public void sort()
     {
         if (this.klasiraneto.Count == 0)
@@ -51,14 +57,17 @@ public class klasirane
             Console.WriteLine();
             return;
         }
+
         Sort();
         Console.WriteLine("Scoreboard:");
+
         for (int index = 0; index < this.klasiraneto.Count; index++)
         {
             string name = this.klasiraneto[index].Key;
             int attempts = this.klasiraneto[index].Value;
             Console.WriteLine("{0}. {1} --> {2} guesses", index + 1, name, attempts);
         }
+
         Console.WriteLine();
     }
 }
